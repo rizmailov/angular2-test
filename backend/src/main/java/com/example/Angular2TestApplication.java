2dd58cc194ffca59032485c7a570ad2ffd5a5fc0
Ramil_Izmaylov <Ramil_Izmaylov@epam.com>
2017-04-14 10:08:23 +0300

    generated frontend

---
+++ backend/src/main/java/com/example/Angular2TestApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Angular2TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Angular2TestApplication.class, args);
+	}
+}
